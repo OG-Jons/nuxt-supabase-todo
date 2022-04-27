5b393922f859a4fa6aa6b7ae2aeb893439a99203
OG-Jons <jonas@marschall.pro>
2022-04-27 14:04:55 +0200

    stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        supabaseKey: process.env.SUPABASE_KEY,
+        public: {
+            supabaseUrl: process.env.SUPABASE_URL,
+        }
+    }
 })
