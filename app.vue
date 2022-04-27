056e1bc9f1670f27506511478751c76785e3b609
OG-Jons <jonas@marschall.pro>
2022-04-27 15:13:32 +0200

    added stuff

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    This is the index pagers
+  </div>
+</template>
+
+<script setup>
+import { createClient } from '@supabase/supabase-js/dist/main/index.js'
+
+const config = useRuntimeConfig();
+
+const supabaseUrl = config.public.supabaseUrl
+const supabaseKey = config.public.supabasePublicKey
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+console.log(supabase)
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     runtimeConfig: {
-        supabaseKey: process.env.SUPABASE_KEY,
         public: {
+            supabasePublicKey: process.env.SUPABASE_PUBLIC_KEY,
             supabaseUrl: process.env.SUPABASE_URL,
         }
     }
